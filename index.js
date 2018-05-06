4c089de300f62e2f3356b23ed83d0d443f95afbd
Pauteau <30441088+Pauteau@users.noreply.github.com>
2018-05-06 17:28:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client()
 
-bot.login('Mzc4ODM4MDIxMDQ0MzA1OTMy.Dcn3Ew.8rgq9byGctQa8SaSj4lK7OitNTI')
+bot.login('Mzc4ODM4MDIxMDQ0MzA1OTMy.Dcn3Ew.8rgq9byGctQa8SaSj4lK7OitNTI');
 
 bot.on('message', function (message) {
 if (message.content ==='d/help') {
